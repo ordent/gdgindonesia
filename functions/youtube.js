e70e4b5eb97ab6da453a10871ee277bd302d5057
Krisna Fathurahman <krisna.fathurahman@gmail.com>
2020-10-16 19:09:50 +0700

    Fixing lambda header

--- functions/youtube.js
+++ functions/youtube.js
@@ -11,6 +11,11 @@ exports.handler = async (event, _, callback) => {
   const url = `https://www.youtube.com/live_chat?v=${video}&embed_domain=${domain}`
   const result = await axios.get(url);
   const headers = { ...result.headers, 'x-frame-options': undefined };
+  console.log({
+    statusCode: result.status,
+    headers,
+    body: result.data
+  })
   callback(null, {
     statusCode: result.status,
     headers: headers,
